3830cceb57ba8f6e63efbab84f990e7226724195
孙皓 <shsgear@qq.com>
2018-10-09 21:04:15 +0800

    rename jsx

---
+++ comment-app/src/CommentApp.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommmentList';
+
+class CommentApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      comments: [
+        {
+          name: 'ETH',
+          msg: 'Ethereum',
+        },
+        {
+          name: 'BTC',
+          msg: 'Bitcoin',
+        },
+      ],
+    }
+  }
+  handleSubmitComment(comment) {
+    this.setState({
+      comments: [comment, ...this.state.comments],
+    })
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <CommentInput onSubmit={this.handleSubmitComment.bind(this)}></CommentInput>
+        <CommentList comments={this.state.comments}></CommentList>
+        <Post></Post>
+        <Ref></Ref>
+      </div>
+    )
+  }
+}
+function getPostData() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(12);
+    }, 1000)
+  })
+}
+class Post extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: '',
+    }
+  }
+  componentWillMount () {
+    this.get();
+  }
+  componentDidMount() {
+    console.log(this.button);
+  }
+  async get() {
+    this.setState({
+      content: '数据加载中...',
+    })
+    
+    const content = await getPostData();
+    this.setState({
+      content
+    })
+  }
+  render () {
+    return (
+      <div>
+        <div className='post-content'>
+         { this.state.content }
+        </div>
+        <button ref={(btn) => {this.button=btn}} onClick={() => {this.get()}}>刷新</button>
+      </div>
+    )
+  }
+}
+
+class Ref extends Component {
+  getHeight() {
+   const height = window.getComputedStyle(this.p, null).height;
+   console.log(height)
+  }
+  static defaultProps = {
+    content: '撒大撒大所大所'
+  }
+  render () {
+    return (
+    <p ref={(p) => {this.p = p}} onClick={() => {this.getHeight()}}>{this.props.content}</p>
+    )
+  }
+}
+export default CommentApp;
